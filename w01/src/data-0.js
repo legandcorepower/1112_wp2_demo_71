a4f347038aa2ea7afc04fc0d7de2a6393a34ebf2
legandcorepower <fgh50608@gmail.com>
2023-02-22 21:27:19 +0800

    w02

---
+++ w01/src/data-0.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: 1,
+    title: 'Samsung Galaxy S7',
+    price: 399.99,
+    img:
+      'https://sjpcedtwrnasasskzklq.supabase.co/storage/v1/object/sign/cart-71/cart-71/S7.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYXJ0LTcxL2NhcnQtNzEvUzcucG5nIiwiaWF0IjoxNjc2NDYwMDYxLCJleHAiOjE3MDc5OTYwNjF9.fzXBitUj9norDZ86cmJVFIWvB5AajOElSsk6mC_71BI&t=2023-02-15T11%3A21%3A01.299Z',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'google pixel ',
+    price: 499.99,
+    img:
+      'https://sjpcedtwrnasasskzklq.supabase.co/storage/v1/object/sign/cart-71/cart-71/Pixel.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYXJ0LTcxL2NhcnQtNzEvUGl4ZWwucG5nIiwiaWF0IjoxNjc2NDYwMTMyLCJleHAiOjE3MDc5OTYxMzJ9.Htuq6n98mM5RtF2ITI8cz3wog7refepkCPptvFoDDcY&t=2023-02-15T11%3A22%3A12.809Z',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Xiaomi Redmi Note 2',
+    price: 699.99,
+    img:
+      'https://sjpcedtwrnasasskzklq.supabase.co/storage/v1/object/sign/cart-71/cart-71/Pixel.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYXJ0LTcxL2NhcnQtNzEvUGl4ZWwucG5nIiwiaWF0IjoxNjc2NDYwMTMyLCJleHAiOjE3MDc5OTYxMzJ9.Htuq6n98mM5RtF2ITI8cz3wog7refepkCPptvFoDDcY&t=2023-02-15T11%3A22%3A12.809Z',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Samsung Galaxy S8',
+    price: 599.99,
+    img:
+      'https://sjpcedtwrnasasskzklq.supabase.co/storage/v1/object/sign/cart-71/cart-71/S8_o.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYXJ0LTcxL2NhcnQtNzEvUzhfby5wbmciLCJpYXQiOjE2NzY0NjAxOTgsImV4cCI6MTcwNzk5NjE5OH0.Nr8qiW59aXHP4VfJXTkANzBZvufZ5qOTNoifL1-PyIk&t=2023-02-15T11%3A23%3A18.907Z',
+    amount: 1,
+  }
+];
