81059b79b06c7d8bba141eeb756dd7be47842276
legandcorepower <37468541+legandcorepower@users.noreply.github.com>
2025-05-13 10:45:23 +0800

    Update apiCard2Router_71.js mark ID to 71

--- routes/api/apiCard2Router_71.js
+++ routes/api/apiCard2Router_71.js
@@ -8,7 +8,7 @@ router.get('/', async function(req, res, next) {
   try{
     const results = await db.query(`select * from card_71`);
     //console.log('json data',JSON.stringify(results.rows));
-    //res.render('card2_71/index', {data:results.rows,name:'鄭凱鴻' ,id: '207410571' });
+    //res.render('card2_71/index', {data:results.rows,name:'鄭凱鴻' ,id: '71' });
     res.json(results.rows);
 }catch(error){
   console.log(error);
